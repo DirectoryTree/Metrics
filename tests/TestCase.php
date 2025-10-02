2a44bb105ee92991774464c57e9ee84d0747a119
Steve Bauman <steven_bauman@outlook.com>
2025-10-02 00:21:51 -0400

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,12 +9,26 @@ use function Orchestra\Testbench\laravel_migration_path;
 
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * Define environment setup.
+     */
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('database.default', 'testing');
+    }
+
+    /**
+     * Define database migrations.
+     */
     protected function defineDatabaseMigrations(): void
     {
         $this->loadMigrationsFrom(laravel_migration_path('/'));
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
+    /**
+     * Get the package providers.
+     */
     protected function getPackageProviders($app): array
     {
         return [MetricServiceProvider::class];
