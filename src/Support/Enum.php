ac039f6803a9f258219be9d6e4b44198a9411391
Steve Bauman <steven_bauman@outlook.com>
2025-10-13 13:38:16 -0400

    Add Enum value helper

---
+++ src/Support/Enum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DirectoryTree\Metrics\Support;
+
+use BackedEnum;
+use UnitEnum;
+
+class Enum
+{
+    /**
+     * Get the value of the enum.
+     */
+    public static function value(mixed $value, mixed $default = null): string|int|null
+    {
+        return match (true) {
+            $value instanceof BackedEnum => $value->value,
+            $value instanceof UnitEnum => $value->name,
+            default => $value ?? value($default),
+        };
+    }
+}
